09ff3ca7b9811cc84d32f63673f5b42ad2b0670c
ashishsen123 <ashish.saiwal@gmail.com>
2014-04-22 17:28:44 +0530

    updated theme

---
+++ page-templates/front-page.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Template Name: Front Page Template
+ *
+ * Description: Displays a full-width front page. The front page template provides an optional
+ * banner section that allows for highlighting a key message. It can contain up to 2 widget areas,
+ * in one or two columns. These widget areas are dynamic so if only one widget is used, it will be
+ * displayed in one column. If two are used, then they will be displayed over 2 columns.
+ * There are also four front page only widgets displayed just beneath the main content. Like the
+ * banner widgets, they will be displayed in anywhere from one to four columns, depending on
+ * how many widgets are active.
+ *
+ * @package Awesome
+ * @since Awesome 1.0
+ */
+
+get_header(); ?>
+
+	<div id="bannercontainer">
+		<div class="banner row">
+			<?php if ( is_front_page() ) {
+				// Count how many banner sidebars are active so we can work out how many containers we need
+				$bannerSidebars = 0;
+				for ( $x=1; $x<=2; $x++ ) {
+					if ( is_active_sidebar( 'frontpage-banner' . $x ) ) {
+						$bannerSidebars++;
+					}
+				}
+
+				// If there's one or more one active sidebars, create a row and add them
+				if ( $bannerSidebars > 0 ) { ?>
+					<?php
+					// Work out the container class name based on the number of active banner sidebars
+					$containerClass = "grid_" . 12 / $bannerSidebars . "_of_12";
+
+					// Display the active banner sidebars
+					for ( $x=1; $x<=2; $x++ ) {
+						if ( is_active_sidebar( 'frontpage-banner'. $x ) ) { ?>
+							<div class="col <?php echo $containerClass?>">
+								<div class="widget-area" role="complementary">
+									<?php dynamic_sidebar( 'frontpage-banner'. $x ); ?>
+								</div> <!-- /.widget-area -->
+							</div> <!-- /.col.<?php echo $containerClass?> -->
+						<?php }
+					} ?>
+
+				<?php }
+			} ?>
+		</div> <!-- /.banner.row -->
+	</div> <!-- /#bannercontainer -->
+	
+	
+
+	<div id="maincontentcontainer">
+	
+	<?php get_sidebar( 'front' ); ?>
+
+<?php get_footer(); ?>
---
+++ sidebar-front.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The sidebar containing the front page widget areas.
+ * If there are no active widgets, the sidebar will be hidden completely.
+ *
+ * @package Awesome
+ * @since Awesome 1.0
+ */
+?>
+	<?php
+	// Count how many front page sidebars are active so we can work out how many containers we need
+	$footerSidebars = 0;
+	for ( $x=1; $x<=4; $x++ ) {
+		if ( is_active_sidebar( 'sidebar-homepage' . $x ) ) {
+			$footerSidebars++;
+		}
+	}
+
+	// If there's one or more one active sidebars, create a row and add them
+	if ( $footerSidebars > 0 ) { ?>
+		<div id="secondary" class="home-sidebar row">
+			<?php
+			// Work out the container class name based on the number of active front page sidebars
+			$containerClass = "grid_" . 12 / $footerSidebars . "_of_12";
+
+			// Display the active front page sidebars
+			for ( $x=1; $x<=4; $x++ ) {
+				if ( is_active_sidebar( 'sidebar-homepage'.  $x ) ) { ?>
+					<div class="col <?php echo $containerClass?>">
+						<div class="widget-area" role="complementary">
+							<?php dynamic_sidebar( 'sidebar-homepage'.  $x ); ?>
+						</div> <!-- #widget-area -->
+					</div> <!-- /.col.<?php echo $containerClass?> -->
+				<?php }
+			} ?>
+		</div> <!-- /#secondary.row -->
+		<div class="main-banner">
+	<div class="main-widget-banner">
+	<p>Tatva is a rock solid mobile responsive WordPress theme loaded with all essential features of a business website.</p>
+	<img src="http://1.gravatar.com/avatar/596dddfd1c9dcb2f15e7dab46ea838f9?s=100&d=http%…2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D10"/>
+	<p class=name>punnet sahlot</p>
+	</div>
+	</div>
+
+	<?php } ?>
